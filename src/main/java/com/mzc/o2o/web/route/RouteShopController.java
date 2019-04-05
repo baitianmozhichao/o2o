608ad8c4a2b77cca7984ead7d4795f3b8b77dbe4
979541020@qq.com <585762011mzc>
2019-04-05 22:08:03 +0800

    使用后端路由

---
+++ src/main/java/com/mzc/o2o/web/route/RouteShopController.java
@@ -0,0 +1,18 @@
+package com.mzc.o2o.web.route;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @auther: mzc
+ */
+@Controller
+@RequestMapping("/route/shop")
+public class RouteShopController {
+
+    @GetMapping("/shopReg")
+    public String shopReg(){
+        return "shop/shopRegistry";
+    }
+}
